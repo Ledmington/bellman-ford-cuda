441e60e09cda40da0d1b44003a646173fe718e82
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-08-08 11:25:41 +0200

    Modificato graphgen

--- src/graphgen.c
+++ src/graphgen.c
@@ -1,28 +1,51 @@
 /*
+    CUDA implementation of the Bellman-Ford's algorithm
+    Copyright (C) 2021  Filippo Barbari
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+/*
     graphgen.c
 
-    Genera un grafo indiretto non pesato casuale secondo il modello Erdos-Renyi.
+    Generates an undirected unweighted random graph based on the Erdos-Renyi model.
     
-    L'output è formattato come segue:
-    la prima riga contiene un singolo numero, N, il numero di nodi del grafo,
-    dopodichè viene stampata la matrice di adiacenza del grafo, N righe con
-    N valori ciascuna separati da spazi, se il valore è 0 l'arco non esiste,
-    1 se l'arco esiste.
+    The output is formatted as follows:
+    the first line contains three numbers: |n_nodes| |n_arcs| 1
+    the following |n_arcs| lines contain:
+    |source_node_index| |destination_node_index| 1.0
 
-    Come compilare:
+    Node indexes are 0-based.
+
+    To compile:
     gcc -std=c99 -Wall -Wpedantic graphgen.c -o graphgen
 
-    Come eseguire
-    ./graphgen N p > graph.txt
+    To run:
+    ./graphgen N p [min-weight max-weight] > graph.txt
 
-    N indica il numero di nodi del grafo da generare,
-    p è la probabilità con cui generare gli archi.
+    N is the number of nodes in the graph,
+    p is the probability to generate an arc.
+
+    If specified, each arc will have a weight in [min_weight; max_weight).
+    Otherwise, each arc is generated with weight 1.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <assert.h>
+#include <math.h>
 
 double randab(double a, double b) {
     return (double)rand() / (double)RAND_MAX * (b-a) + a;
@@ -31,44 +54,68 @@ double randab(double a, double b) {
 int main ( int argc, char** argv ) {
     srand(time(NULL));
 
-    if(argc != 3) {
-        fprintf(stderr, "Utilizzo: \"%s N p\"\n", argv[0]);
-        fprintf(stderr, " N e' il numero di nodi del grafo\n");
-        fprintf(stderr, " p e' la probabilita' con cui generare gli archi\n");
+    if(argc != 3 && argc != 5) {
+        fprintf(stderr, "Usage: \"%s N p [min_weight max_weight]\"\n", argv[0]);
+        fprintf(stderr, " N is the number of nodes in the graph\n");
+        fprintf(stderr, " p is the probability to generate an arc\n");
+        fprintf(stderr, " If specified, each arc will have a weight in [min_weight; max_weight).\n"
+                        " Otherwise, each arc is generated with weight 1.\n");
         return EXIT_FAILURE;
     }
 
     unsigned int N = atoi(argv[1]);
     double p = strtod(argv[2], NULL);
+    double min_weight = 1.0f;
+    double max_weight = 1.0f;
+
+    if(argc == 5) {
+        min_weight = strtod(argv[3], NULL);
+        max_weight = strtod(argv[4], NULL);
+    }
 
     fprintf(stderr, "N: %u\np: %f\n", N, p);
-    unsigned int n_archi = 0;
+    if(argc == 5) {
+        fprintf(stderr, "Weights in [%f; %f)\n", min_weight, max_weight);
+    }
 
-    bool g[N][N];
+    unsigned int n_archi = 0;
+    double **g;
+
+    g = (double**) malloc(N * sizeof(double*));
+    assert(g);
+    for(unsigned int i=0; i<N; i++) {
+        g[i] = (double*) malloc(N * sizeof(double));
+        assert(g[i]);
+    }
 
     for(unsigned int i=0; i<N; i++) {
-        g[i][i] = false;
+        g[i][i] = HUGE_VAL;
         for(unsigned int j=i+1; j<N; j++) {
             if(randab(0.0, 1.0) <= p) {
-                g[i][j] = g[j][i] = true;
+                g[i][j] = g[j][i] = randab(min_weight, max_weight);
                 n_archi++;
             }
             else {
-                g[i][j] = g[j][i] = false;
+                g[i][j] = g[j][i] = HUGE_VAL;
             }
         }
     }
 
-    fprintf(stderr, "n. archi (creati / totali): %u / %u\n", n_archi, N*(N-1)/2);
+    fprintf(stderr, "n. arcs (created / total): %u / %u\n", n_archi, N*(N-1)/2);
 
-    printf("%u\n", N);
+    printf("%u %u 1\n", N, n_archi);
     for(unsigned int i=0; i<N; i++) {
-        for(unsigned int j=0; j<N; j++) {
-            printf("%u", g[i][j] ? 1 : 0);
-            if(j != N-1) printf(" ");
+        for(unsigned int j=i+1; j<N; j++) {
+            if(!isinf(g[i][j])) {
+                printf("%u %u %f\n", i, j, g[i][j]);
+            }
         }
-        printf("\n");
     }
+
+    for(unsigned int i=0; i<N; i++) {
+        free(g[i]);
+    }
+    free(g);
 
     return EXIT_SUCCESS;
 }
