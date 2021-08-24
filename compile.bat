5fb2d3bb58deb717a0f09029155ffd1730e47437
Ledmington <68538713+Ledmington@users.noreply.github.com>
2021-08-24 09:49:57 +0200

    Aggiunta scelta architettura CUDA a compile.bat e compile.sh

--- compile.bat
+++ compile.bat
@@ -2,20 +2,21 @@
 
 if not exist bin mkdir bin
 
-if [%1] == [] (
+if [%2] == [] (
     echo Please type the name of the file, without extension, to be compiled
     echo or type "all" to compile all .cu files.
+    echo Then type the CUDA architecture you want to compile for.
     exit /b 1
 )
 
 if "%1" == "all" (
 	for /f tokens^=* %%A in ('where "src:*.cu"') do (
 		if exist bin\%%~nA.exe del bin\%%~nA*
-		nvcc -Wno-deprecated-gpu-targets -arch=compute_50 %%A -o bin\%%~nA.exe
+		nvcc -Wno-deprecated-gpu-targets -arch=%2 -rdc=true %%A -o bin\%%~nA.exe
 	)
 	exit /b 1
 )
 
 if exist bin\%1.exe del bin\%1*
-nvcc -Wno-deprecated-gpu-targets -arch=compute_50 src\%1.cu -o bin\%1.exe
+nvcc -Wno-deprecated-gpu-targets -arch=%2 -rdc=true src\%1.cu -o bin\%1.exe
 exit /b 1
--- compile.sh
+++ compile.sh
@@ -1,8 +1,9 @@
 if [ ! -d bin ] ; then mkdir bin ; fi
 
-if [ $# -eq 0 ] ; then
+if [ $# -lt 2 ] ; then
     echo Please type the name of the file, without extension, to be compiled
     echo or type "all" to compile all .cu files.
+    echo Then type the CUDA architecture you want to compile for.
     exit 1
 fi
 
@@ -11,13 +12,13 @@ if [ "$1" == "all" ] ; then
 		F=${F##*/}
 		echo Compiling ${F}...
 		if [ -f bin/${F%.*}.exe ] ; then rm bin/${F%.*}* ; fi
-		nvcc -Wno-deprecated-gpu-targets -arch=compute_50 src/${F} -o bin/${F%.*}.exe ;
+		nvcc -Wno-deprecated-gpu-targets -arch=$2 -rdc=true src/${F} -o bin/${F%.*}.exe ;
 		if [[ $? == 0 ]] ; then echo Compilation successful ; fi
 	done
 	exit 1
 fi
 
 if [ -f bin/$1.exe ] ; then rm bin/$1* ; fi
-nvcc -Wno-deprecated-gpu-targets -arch=compute_50 src/$1.cu -o bin/$1.exe
+nvcc -Wno-deprecated-gpu-targets -arch=$2 -rdc=true src/$1.cu -o bin/$1.exe
 if [[ $? == 0 ]] ; then echo Compilation successful ; fi
 exit 1
